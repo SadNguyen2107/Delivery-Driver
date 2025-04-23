f07b3c59e3bc4d8179121020119178426f75838f
SadNguyen2107 <126353303+SadNguyen2107@users.noreply.github.com>
2025-04-23 22:49:46 +0700

    Add Scripts to move the player
    - Move Forward
    - Move Backward
    - Turn Left (when moving)
    - Turn Right (when moving)

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+// PlayerController.cs
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerMovement _playerMovement;
+    InputAction _playerMoveAction;
+
+    void Awake()
+    {
+        FindActions();
+    }
+    void Start()
+    {
+        if (!TryGetComponent(out _playerMovement))
+        {
+            Debug.LogError("PlayerMovement component not found on the player object.");
+            return;
+        }
+    }
+
+    void OnEnable()
+    {
+        _playerMoveAction?.Enable();
+    }
+
+    void OnDisable()
+    {
+        _playerMoveAction?.Disable();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = _playerMoveAction.ReadValue<Vector2>();
+        // Assuming you have a PlayerMovement component to handle movement
+        _playerMovement.SetMoveInput(moveInput);
+    }
+
+    public void FindActions()
+    {
+        _playerMoveAction = InputSystem.actions.FindAction("Player/Move") ??
+            throw new System.Exception("Player/Move action not found in InputActions.");
+    }
+}
